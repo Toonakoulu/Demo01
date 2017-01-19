d73115f0e3e9dde89e3458eea573e12c56647174
Tony Vähävuori <h9234@student.jamk.fi>
2017-01-19 15:18:35 +0200

    Lisää tehtävää.

---
+++ teht10/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teht10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] luvut = new int[9];
+
+            luvut[0] = 1;
+            luvut[1] = 2;
+            luvut[2] = 33;
+            luvut[3] = 44;
+            luvut[4] = 55;
+            luvut[5] = 68;
+            luvut[6] = 77;
+            luvut[7] = 96;
+            luvut[8] = 100;
+
+            for (int i = 0; i < luvut.Length; luvut[i]++) 
+
+
+
+
+
+            // wait enter
+            Console.ReadLine();
+        }
+    }
+}
