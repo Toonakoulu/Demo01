2fd691cb90b8e0f2f2eef5950b965deff0aa6c62
Tony Vähävuori <h9234@student.jamk.fi>
2017-01-22 15:41:46 +0200

    Tehtäviää.

---
+++ teht12/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teht12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] luvut = new int[5];
+            int luku0, luku1, luku2, luku3, luku4;
+
+            // Kysytään lukuja
+            Console.Write("Anna luku > ");
+            luku0 = int.Parse(Console.ReadLine());
+            luvut[0] = luku0;
+            Console.Write("Anna luku > ");
+            luku1 = int.Parse(Console.ReadLine());
+            luvut[1] = luku1;
+            Console.Write("Anna luku > ");
+            luku2 = int.Parse(Console.ReadLine());
+            luvut[2] = luku2;
+            Console.Write("Anna luku > ");
+            luku3 = int.Parse(Console.ReadLine());
+            luvut[3] = luku3;
+            Console.Write("Anna luku > ");
+            luku4 = int.Parse(Console.ReadLine());
+            luvut[4] = luku4;
+
+            for (int i = 4; i < luvut.Length; i--) Console.WriteLine(luvut[i]);
+                            
+
+
+            // wait enter
+            Console.ReadLine();
+        }
+    }
+}
