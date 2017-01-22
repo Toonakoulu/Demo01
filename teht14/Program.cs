af68eac8485982f586e5da4af84fae728917a90e
Tony Vähävuori <h9234@student.jamk.fi>
2017-01-22 17:04:21 +0200

    Lisää tehtäviä.

---
+++ teht14/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teht14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            { // ikuinen silmukka
+                Console.Write("Anna luku > ", "Paina 6 jotta pääset pois");
+                int asana = int.Parse(Console.ReadLine());
+                if (asana >= 6) break;
+            }
+            for (int i = 0; i < 10; i++) Console.Write("*");
+
+
+
+
+            // wait enter
+            Console.ReadLine();
+        }
+    }
+}
---
+++ teht16/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teht16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int luku, sLuku, kierros = 0;
+            Random rand = new Random();
+            sLuku = rand.Next(100);
+
+            do
+            {
+                Console.Write("Arvaa luku > ");
+                luku = int.Parse(Console.ReadLine());
+
+                kierros++;
+                if (luku < sLuku) Console.WriteLine("Luku on suurempi.");
+                else if (luku > sLuku) Console.WriteLine("Luku on pienempi.");
+                else if (luku == sLuku) Console.WriteLine("Hienoa, arvasit luvun " + kierros + ". kerralla.");
+
+            } while (luku != sLuku);
+
+
+            // wait enter
+            Console.ReadLine();
+        }
+    }
+}
